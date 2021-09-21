a11b9eb3e890cdddaf5286049e58a7db9a68b1c5
Krist14n <kristian.tapia01@gmail.com>
2021-09-20 22:57:05 -0700

    add song playing functionality

--- src/components/SongPlayer.js
+++ src/components/SongPlayer.js
@@ -5,6 +5,7 @@ import React from 'react'
 import { SongContext } from '../App'
 import { GET_QUEUED_SONGS } from '../graphql/queries'
 import QueuedSongList from './QueuedSongList'
+import ReactPlayer from 'react-player';
 
 const useStyles = makeStyles(theme => ({
     container: {
@@ -37,12 +38,65 @@ const useStyles = makeStyles(theme => ({
 
 function SongPlayer() {
     const {data} = useQuery(GET_QUEUED_SONGS)
+    const reactPlayerRef = React.useRef()
     const {state, dispatch} = React.useContext(SongContext)
+    const [played, setPlayed] = React.useState(0)
+    const [seeking, setSeeking] = React.useState(false)
+    const [playedSeconds, setPlayedSeconds] = React.useState(0)
+    const [positionInQueue, setPositionInQueue] = React.useState(0)
     const classes = useStyles()
+
+    React.useEffect(() => {
+       const songIndex =  data.queue.findIndex(song => song.id === state.song.id)
+       setPositionInQueue(songIndex)
+    }, [data.queue, state.song.id])
+
+    React.useEffect(() => {
+        const nextSong = data.queue[positionInQueue + 1]
+        if(nextSong && played >= 0.99){
+            setPlayed(0)
+            dispatch({type: "SET_SONG", payload: {song: nextSong} })
+        }
+    }, [data.queue, played, dispatch, positionInQueue])
 
     function handleTogglePlay(params) {
         dispatch(state.isPlaying ? {type: 'PAUSE_SONG'} : {type: 'PLAY_SONG'})
     }
+
+    function handleProgressChange(event, newValue) {
+        setPlayed(newValue);
+    }
+
+    function handleSeekMouseDown() {
+        setSeeking(true)
+        
+    }
+
+    function handleSeekMouseUp() {
+        setSeeking(false)
+        reactPlayerRef.current.seekTo(played)
+    }
+
+    function formatDuration(seconds) {
+        return new Date(seconds*1000).toISOString().substr(11,8)
+    }
+
+     function handlePlayNextSong() {
+        const nextSong = data.queue[positionInQueue + 1]
+        if(nextSong) {
+            setPlayed(0)
+            dispatch({type: "SET_SONG", payload: {song: nextSong} })
+        }
+    }
+
+    function handlePlayPrevSong() {
+        const prevSong = data.queue[positionInQueue - 1]
+        if(prevSong) {
+            setPlayed(0)
+            dispatch({type: "SET_SONG", payload: {song: prevSong} })
+        }
+    }
+
     return (
         <>
             <Card variant="outlined" className={classes.container}>
@@ -56,21 +110,37 @@ function SongPlayer() {
                         </Typography>
                     </CardContent>
                     <div className={classes.controls}>
-                        <IconButton>
+                        <IconButton onClick={handlePlayPrevSong}>
                             <SkipPrevious></SkipPrevious>
                         </IconButton>
                         <IconButton onClick={handleTogglePlay}>
                             { state.isPlaying ? <Pause className={classes.playIcon}/> : <PlayArrow className={classes.playIcon}/> }
                         </IconButton>
-                        <IconButton>
+                        <IconButton onClick={handlePlayNextSong}>
                             <SkipNext></SkipNext>
                         </IconButton>
                         <Typography variant="subtitle1" component="p" color="textSecondary">
-                            00:00:00
+                           {formatDuration(playedSeconds)}
                         </Typography>
                     </div>
-                    <Slider type="range" min={0} max={1} step={0.01} ></Slider>
+                    <Slider
+                    onMouseDown={handleSeekMouseDown}
+                    onMouseUp={handleSeekMouseUp}
+                    onChange={handleProgressChange}
+                    value={played} type="range" min={0} max={1} step={0.01} ></Slider>
                 </div>
+                <ReactPlayer
+                    ref={reactPlayerRef}
+                    onProgress={({played, playedSeconds}) => {
+                        if(!seeking) {
+                            setPlayed(played)
+                            setPlayedSeconds(playedSeconds)
+                        }
+                    }} 
+                    url={state.song.url}
+                    playing={state.isPlaying} 
+                    hidden 
+                />
                 <CardMedia image={state.song.thumbnail} className={classes.thumbnail}/>
             </Card>
             <QueuedSongList queue={data.queue} />
